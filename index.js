30e2565b2c18a051097b7c98000ddf6992a84eaf
teamamisimedishvili <127042618+teamamisimedishvili@users.noreply.github.com>
2024-02-21 11:15:55 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+class Person {
+    name;
+    lastname;
+    age;
+
+
+    per_son() {
+
+        document.writeln(this.name);
+        document.writeln(this.lastname);
+        document.write(this.age);
+    }
+}
+
+     var obieqti = new Person ();
+
+     obieqti.name = "mate";
+     obieqti.lastname = "matia";
+     obieqti.age = 20
+
+     obieqti.per_son()
+
